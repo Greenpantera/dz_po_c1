6f2955a4fba97e0ea8e979b81121f8cb02c88c80
Ekaterina <greenpantera@rambler.ru>
2023-07-09 21:35:12 +0500

    Программа по выявлению чётного числа

---
+++ sadatscha3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+if (b<a)
+{
+    Console.Write($"Максимальным числом будет {a}"); 
+    a = max;
+}
+else
+{
+   Console.Write($"Максимальным числом будет {b}"); 
+   b = max;
+}
+if (c>max)
+{
+    c = max;
+    Console.Write($"Максимальным числом будет {c}"); 
+}
